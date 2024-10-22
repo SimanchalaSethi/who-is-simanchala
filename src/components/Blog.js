97976175f7714e0a490cbf3a60877a9aaaa1e0f8
SimanchalaSethi <simanchala.js@gmail.com>
2024-10-22 22:21:28 +0530

    change blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './Blog.css';
-import githubFlowImage from '../assets/gitflow.png'; 
+
+Modal.setAppElement('#root'); // Replace '#root' with your app's root element if different
 
 function Blog() {
-  const [posts, setPosts] = useState([
+  const [posts] = useState([
     {
       title: 'Efficient Code Management with a Structured Git Workflow',
+      summary: 'This post covers efficient code management with a structured Git workflow, including branching strategy, code writing, and more.',
       content: `<p><strong>Branching Strategy:</strong></p>
                 <p>- <strong>Main Branches:</strong> We operate with three main branches - dev, rel, and main.</p>
                 <p>- <strong>Feature Branches:</strong> Team members pull from the dev branch and create feature branches based on the specific APIs they are working on.</p>
@@ -18,8 +21,21 @@ function Blog() {
                 <p>- <strong>Integration:</strong> Once testing is successful, I merge the changes into the rel branch where frontend and backend are integrated.</p>
                 <p>- <strong>Client Approval:</strong> Our testing team validates the integrated application and seeks client approval.</p>
                 <p>- <strong>Final Merge:</strong> Upon client approval, the code is merged into the main branch, marking it ready for production.</p>`,
-    }
+    },
   ]);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const openModal = (post) => {
+    setSelectedPost(post);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedPost(null);
+  };
 
   return (
     <div id="blog">
@@ -27,9 +43,19 @@ function Blog() {
       {posts.map((post, index) => (
         <div key={index} className="blog-post">
           <h2>{post.title}</h2>
-          <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+          <p>{post.summary}</p>
+          <button onClick={() => openModal(post)}>Read Details</button>
         </div>
       ))}
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Blog Post Details">
+        {selectedPost && (
+          <div>
+            <h2>{selectedPost.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: selectedPost.content }}></div>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
 
 function Main() {
-    return (
-        <main className="Main">
-            <section id="about">
-                <h2>About Me</h2>
-                <p>I am a software developer with expertise in Node.js, React, MongoDB, Docker, Jenkins, microservices architecture, and AWS. I enjoy building scalable and efficient applications.</p>
-            </section>
-            <section id="projects">
-                <h2>Projects</h2>
-                <div className="project">
-                <h3>Rating Management in Hotel</h3>
-                <p>This project involved developing a comprehensive rating management system for a hotel chain. The system allows users to submit, view, and manage ratings for various services provided by the hotel. Key features include real-time updates, data visualization, and a user-friendly interface.</p>
-                    <a href="https://github.com/SimanchalaSethi/Hotel-User-Rating-Management" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="Main">
+      <section id="about">
+        <h2>About Me</h2>
+        <p>
+          I am a software developer with expertise in Node.js, React, MongoDB,
+          Docker, Jenkins, microservices architecture, and AWS. I enjoy building
+          scalable and efficient applications.
+        </p>
+      </section>
+      <section id="projects">
+        <h2>Projects</h2>
+        <div class="projects-container">
+          <div class="project">
+            <h3>Rating Management in Hotel</h3>
+            <p>
+              This project involved developing a comprehensive rating management
+              system for a hotel chain. The system allows users to submit, view,
+              and manage ratings for various services provided by the hotel. Key
+              features include real-time updates, data visualization, and a
+              user-friendly interface.
+            </p>
+            <a
+              href="https://github.com/SimanchalaSethi/Hotel-User-Rating-Management"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View on GitHub
+            </a>
+          </div>
+          <div class="project">
+            <h3>Banking Application</h3>
+            <p>
+              This project is a robust banking application developed using
+              Spring Boot, JWT tokens for secure authentication, and MySQL for
+              database management. It includes features like user
+              authentication, transaction management, and account services.
+            </p>
+            <a
+              href="https://github.com/SimanchalaSethi/Back-End-Bank-Application.git"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View on GitHub
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
