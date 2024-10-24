0a5b33803f7db6620cd394ef822ee11c3f7b6b78
SimanchalaSethi <simanchala.js@gmail.com>
2024-10-24 22:40:04 +0530

    add contact image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-
-
 import './App.css';
-import Contact from './components/Contact';
-import Blog from './components/Blog/Blog';
+import Contact from './Contact/Contact';
+import Blog from './Blog/Blog';
 import GreetingPopup from './Greeting/GreetingPopup';
 import Services from './Service/Services';
 
