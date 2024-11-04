b15592bbfbdb826528cd720a48efe04c6f92fb1b
SimanchalaSethi <simanchala.js@gmail.com>
2024-11-04 20:57:07 +0530

    change background color

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,39 +14,43 @@ function Main() {
       </section>
       <section id="projects">
         <h2>Projects</h2>
-        <div class="projects-container">
-          <div class="project">
-            <h3>Rating Management in Hotel</h3>
-            <p>
-              This project involved developing a comprehensive rating management
-              system for a hotel chain. The system allows users to submit, view,
-              and manage ratings for various services provided by the hotel. Key
-              features include real-time updates, data visualization, and a
-              user-friendly interface.
-            </p>
-            <a
-              href="https://github.com/SimanchalaSethi/Hotel-User-Rating-Management"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on GitHub
-            </a>
+        <div className="projects-container">
+          <div className="project">
+            <div className="project-content">
+              <h3>Rating Management in Hotel</h3>
+              {/* <p>
+                This project involved developing a comprehensive rating management
+                system for a hotel chain. The system allows users to submit, view,
+                and manage ratings for various services provided by the hotel. Key
+                features include real-time updates, data visualization, and a
+                user-friendly interface.
+              </p> */}
+              <a
+                href="https://github.com/SimanchalaSethi/Hotel-User-Rating-Management"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on GitHub
+              </a>
+            </div>
           </div>
-          <div class="project">
-            <h3>Banking Application</h3>
-            <p>
-              This project is a robust banking application developed using
-              Spring Boot, JWT tokens for secure authentication, and MySQL for
-              database management. It includes features like user
-              authentication, transaction management, and account services.
-            </p>
-            <a
-              href="https://github.com/SimanchalaSethi/Back-End-Bank-Application.git"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on GitHub
-            </a>
+          <div className="project">
+            <div className="project-content">
+              <h3>Banking Application</h3>
+              {/* <p>
+                This project is a robust banking application developed using
+                Spring Boot, JWT tokens for secure authentication, and MySQL for
+                database management. It includes features like user
+                authentication, transaction management, and account services.
+              </p> */}
+              <a
+                href="https://github.com/SimanchalaSethi/Back-End-Bank-Application.git"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on GitHub
+              </a>
+            </div>
           </div>
         </div>
       </section>
